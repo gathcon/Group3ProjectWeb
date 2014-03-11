6316ee73d4e682eb3b84990efc7538d7ec8abd29
ronaldoshaky <ronaldoshaky@yahoo.com>
2014-03-11 19:08:10 +0000

    added jquery

--- src/main/java/jaxrs/Base_DataWS.java
+++ src/main/java/jaxrs/Base_DataWS.java
@@ -9,6 +9,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import model.Base_Data;
 import dao.Base_DataDAO;
@@ -23,6 +25,7 @@ public class Base_DataWS {
 
     @GET
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
     public Base_Data getBase_Data(@PathParam("id") int id) {
         return base_DatasDao.getBase_Data(id);
     }
